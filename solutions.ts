15a0cb589d0a979790a5a3bb4cf41d5945f8232b
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-08 07:43:49 +0600

    problem 2 solution

--- solutions.ts
+++ solutions.ts
@@ -5,3 +5,17 @@ function formatString(input: string, toUpper?: boolean): string {
     return input.toLowerCase();
   }
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const result: { title: string; rating: number }[] = [];
+
+  items.map((item: { title: string; rating: number }) => {
+    if (item.rating >= 4) {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
